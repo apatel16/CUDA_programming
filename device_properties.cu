00a6be8557dab12977f17c962cb42b365cb3eb7f
apatel16 <apatel16@leomail.tamuc.edu>
2019-04-09 22:48:14 -0500

    gpu device properties

---
+++ device_properties.cu
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    void printCudaDeviceProperties(void);
+
+    printCudaDeviceProperties();
+    return 0;
+}
+
+//ToDo - Get the information about gpu device
+void printCudaDeviceProperties(){
+
+
+}
